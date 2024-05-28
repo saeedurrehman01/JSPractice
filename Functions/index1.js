cf4c90aa1ff5be06cc7b7148bae4d1113b6e4d38
saeedurrehman01 <saeedrehman241@gmail.com>
2024-05-28 14:35:08 +0500

    Even Odd exercise completed

--- Functions/index1.js
+++ Functions/index1.js
@@ -25,12 +25,16 @@ number1 = maxNumber(11, 15);
 
 console.log(number1);
 
+// ------------------------------
+
 function isLandscape(width, height) {
   return width > height;
 }
 
 picture = isLandscape(2, 3);
 console.log(picture);
+
+// ------------------------------
 
 function fizzBuzz(input) {
   if (typeof input !== "number") return NaN;
@@ -42,3 +46,32 @@ function fizzBuzz(input) {
 
 const output = fizzBuzz(15);
 console.log(output);
+// ------------------------------
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed <= speedLimit) {
+    console.log("Ok");
+  }
+  let points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= 12) console.log("License Suspended");
+  else console.log("Points", points);
+}
+
+checkSpeed(75);
+// ------------------------------
+
+function showNumbers(limit) {
+  for (i=0; i<=limit; i++){
+    if (i % 2 == 0){
+      console.log(i, "EVEN")
+    }
+    else console.log(i, "ODD")  
+  }
+}
+
+showNumbers (10)
+
+// ------------------------------
