678b61fce0a6bdb765377c66a0b26b16abb4d1c8
saeedurrehman01 <saeedrehman241@gmail.com>
2024-05-28 12:56:23 +0500

    FizzBuzz Done

--- Functions/index1.js
+++ Functions/index1.js
@@ -1,5 +1,44 @@
-let name = "Mosh";
-let age = 30;
-let firstName = undefined;
+// let name = "Mosh";
+// let age = 30;
+// let firstName = undefined;
 
-console.log("Hello World");
+// let person = {
+//   name: "Saeed",
+//   age: 26,
+// };
+
+// // console.log(person);
+
+// person.name = "Rehman";
+
+// // console.log(person);
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log("Hello World", i);
+// }
+
+function maxNumber(a, b) {
+  return a > b ? a : b;
+}
+
+number1 = maxNumber(11, 15);
+
+console.log(number1);
+
+function isLandscape(width, height) {
+  return width > height;
+}
+
+picture = isLandscape(2, 3);
+console.log(picture);
+
+function fizzBuzz(input) {
+  if (typeof input !== "number") return NaN;
+  else if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+  if (input % 5 == 0) return "Buzz";
+  if (input % 3 == 0) return "Fizz";
+  return input;
+}
+
+const output = fizzBuzz(15);
+console.log(output);
